db74161d9b85b0a5cde236f956e88138dfee0dd4
Dasha Kolegova <kolegovadasa2@gmail.com>
2026-05-11 02:42:42 +0700

    Nothing

--- GetTripSystem/obj/Debug/net10.0-windows/GetTripSystem.AssemblyInfo.cs
+++ GetTripSystem/obj/Debug/net10.0-windows/GetTripSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GetTripSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc68faeec4d8cbcfc16fb0cf90cbe52128896865")]
 [assembly: System.Reflection.AssemblyProductAttribute("GetTripSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GetTripSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Tests/UserTests.cs
@@ -0,0 +1,18 @@
+﻿using GetTripSystem.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static GetTripSystem.DAL;
+
+namespace GetTripSystem.Tests
+{
+
+    [TestClass]
+    public sealed class UserTests
+    {
+
+    }
+        
+}
